4c2b75d234ec88437b6a2a0a52d353995555ed9a
MacNera <ale.fog93@yahoo.it>
2015-04-20 09:59:15 +0200

    Aggiunto commento

--- src/it/uniroma3/db/products/Product.java
+++ src/it/uniroma3/db/products/Product.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Column;
-
+// Valerio 
 	@Entity
 	@NamedQuery(name = "findAllProducts", query = "SELECT p FROM Product p")
 	public class Product {
